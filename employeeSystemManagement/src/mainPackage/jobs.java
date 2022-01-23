0fa6669424d3d94a193b0477caa82e167ef3fd85
mmabbasi1374 <mmabbasi1374@yahoo.com>
2022-01-23 18:40:23 +0330

    add files
    add our word file
    add source

---
+++ employeeSystemManagement/src/mainPackage/jobs.java
@@ -0,0 +1,42 @@
+
+package mainPackage;
+
+import java.util.ArrayList;
+
+    public class jobs {
+    //	In the “jobs” class, we have these properties:
+    //jobTitle, salary, Tax
+    //////properties
+
+        private String jobTitle;
+        
+        private Integer salary;
+
+        private Integer tax;
+
+    //////constuct function
+
+
+
+    //////Get
+
+
+
+    /////functions
+    ////this function will send data which we will get from textField(input that is related to add new job) to a XML file and store them.
+    public void sendToXML(){
+
+    }
+    ///we can list our jobs by this function in jobs class.
+
+    public static ArrayList<jobs> jobsList(){
+
+        ArrayList jobsList = new ArrayList();
+
+
+
+        return jobsList;
+        }
+
+    
+}
---
+++ employeeSystemManagement/src/mainPackage/users.java
@@ -0,0 +1,44 @@
+
+package mainPackage;
+
+import java.util.ArrayList;
+
+
+public class users {
+    ///In the “users” class, we have these properties:
+    ///fullName, age, email, ID
+    private String fullName;
+    private Integer age;
+    private String email;
+    private Integer ID;
+    //////constuct function
+
+
+
+    //////Get
+
+
+
+    /////functions
+//this function will send data which we will get from textField(input that is related to sign up(add new user)) to XML file and store them.
+public void sendToXML(){
+
+}
+///we can list our users by this function in users class.
+
+public static ArrayList<users> usersList(){
+
+    ArrayList usersList = new ArrayList();
+
+
+
+    return usersList;
+}
+///this function will be used for delete a user in users class
+public void remove(){
+}
+//we will check if the data which comes from “log in” is the same as  data’s we have already gotten from “sign up” or not 
+public void checkLog(){
+}
+}
+
